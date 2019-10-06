1cb2e24d9f5e6e92281d6d9cb3c702f89d2f2ad6
iiskakov <tradeshells@gmail.com>
2019-10-06 00:40:10 -0700

    hammerspoon config with kitty toggle

---
+++ init.lua
@@ -0,0 +1,21 @@
+cmd_ctrl = {"cmd","ctrl"}
+script = [[ 
+set appName to "kitty"
+set startIt to false
+tell application "System Events"
+	if not (exists process appName) then
+		set startIt to true
+	else if visible of process appName then
+		set visible of process appName to false
+	else
+		set visible of process appName to true
+		set frontmost of process appName to true
+	end if
+end tell
+if startIt then
+	tell application appName to activate
+end if ]]
+hs.hotkey.bind(cmd_ctrl, "j", function()
+  hs.applescript(script)
+end)
+
