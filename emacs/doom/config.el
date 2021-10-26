72ffa15efb3eee68ea7a63535345964e0538003b
Iskander Iskakov <tradeshells@gmail.com>
2021-10-26 10:04:39 +0600

    Found a way to add ascii banner in config

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -113,6 +113,29 @@
 ;; Tide configuring end
 ;;
 
+(defun 42-ftw ()
+  (let* ((banner '(
+"        :::      ::::::::"
+"      :+:      :+:    :+:"
+"    +:+ +:+         +:+  "
+"  +#+  +:+       +#+     "
+"#+#+#+#+#+   +#+         "
+"     #+#    #+#          "
+"    ###   ##########     "))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat line (make-string (max 0 (- longest-line (length line))) 32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'42-ftw)
+
+
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Documets/org")
